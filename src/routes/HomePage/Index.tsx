abe4eb6cecc44bdbb02768c6e58f7fa7137da28a
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-01-25 14:38:04 +0530

    changed styles button and textfield

--- src/routes/HomePage/Index.tsx
+++ src/routes/HomePage/Index.tsx
@@ -10,10 +10,18 @@ const HomePage = () => {
         <Button variant="outlined" color="error">
           outlined
         </Button>
-        <Button variant="text">text</Button>
+        <Button color="success" variant="text">
+          text
+        </Button>
       </Stack>
       <Stack direction={"row"} gap={2}>
-        <TextField label="Default title 1" variant="filled" size="small" />
+        <TextField
+          label="Default title 1"
+          error={true}
+          helperText={"this is error"}
+          variant="filled"
+          size="small"
+        />
         <TextField label="Default title 2" variant="outlined" size="small" />
         <TextField label="Default title 3" variant="standard" size="small" />
       </Stack>
