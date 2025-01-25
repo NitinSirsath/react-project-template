e1f23f927c0802d5d623e2c8af4c61f486d6790d
Nitin Sirsath <73011138+NitinSirsath@users.noreply.github.com>
2025-01-25 16:30:15 +0530

    added header

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,6 +1,7 @@
 import styles from "./AppLayout.module.css";
 import { ReactNode } from "react";
 import { useAuthStore } from "../../services/store/auth/authStore";
+import ResponsiveAppBar from "./header/ResponsiveAppBar";
 // import Header from "../components/header/Header";
 
 interface IProps {
@@ -15,11 +16,11 @@ const AppLayout = ({ children }: IProps) => {
       {isLoggedIn && (
         <div className={styles.headerContainer}>
           {/* <Header /> */}
-          Header
+          <ResponsiveAppBar />
         </div>
       )}
       <main
-        style={{ margin: isLoggedIn ? "100px 17px 10px" : "0px" }}
+        style={{ margin: isLoggedIn ? "20px" : "0px" }}
         className={styles.mainContent}
       >
         {children}
